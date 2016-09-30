9df6ea4d3e66d9034485fcfdd6283b1a5f41c186
fishpaypay <fishpaypay@fanswoo.com>
2016-09-30 17:05:44 +0800

    業績後台系統串聯(未完)

---
+++ controllers/admin/project/sales_target/Sales_target.php
@@ -0,0 +1,260 @@
+<?php
+
+class Customer_Controller extends MY_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('AdminModel');
+        $this->AdminModel->construct(['data' => $this->data, 'file' => __FILE__ ]);
+    }
+
+    public function edit()
+    {
+        $data = $this->AdminModel->get_data(__FUNCTION__);
+
+        $customerid = $this->input->get('customerid');
+
+        $data['Customer'] = new Customer([
+            'db_where_arr' => [
+                'customerid' => $customerid
+            ]
+        ]);
+
+        $data['global']['js'][] = 'tool/ckeditor/ckeditor.js';
+        $data['global']['js'][] = 'tool/jquery-ui-timepicker-addon/script.js';
+        $data['global']['js'][] = 'tool/jquery-ui-timepicker-addon/style.css';
+
+        //輸出模板
+        $this->load->view('admin/'.$data['admin_child_url'], $data);
+    }
+
+    public function edit_post()
+    {
+        $data = $this->AdminModel->get_data(__FUNCTION__);
+
+        //基本post欄位
+        $customerid = $this->input->post('customerid', TRUE);
+        $customer_name = $this->input->post('customer_name', TRUE, '客戶名稱', 'required');
+        $company = $this->input->post('company', TRUE, '公司名稱', 'required');
+        $phone = $this->input->post('phone', TRUE, '手機號碼', 'required');
+        $tel = $this->input->post('tel', TRUE, '公司電話');
+        $email = $this->input->post('email', TRUE, '電子郵件', 'required|valid_email');
+        $address = $this->input->post('address', TRUE, '公司地址', 'required');
+        $wish = $this->input->post('wish','意願',TRUE);
+        $budget_range = $this->input->post('budget_range', TRUE, '預算', 'required');
+        $content = $this->input->post('content', FALSE, '功能需求');
+        $contact_time = $this->input->post('contact_time', TRUE, '最後聯繫時間');
+        $website = $this->input->post('website', TRUE, '公司網址');
+        $prioritynum = $this->input->post('prioritynum', TRUE);
+        $updatetime = $this->input->post('updatetime', TRUE, 名單新增時間);
+        if( !$this->form_validation->check() ) return FALSE;
+
+        //建構Customer物件，並且更新
+        $Customer = new Customer([
+            'customerid' => $customerid,
+            'customer_name' => $customer_name,
+            'company' => $company,
+            'phone' => $phone,
+        	'tel'=> $tel,
+            'email' => $email,
+            'address' => $address, 
+            'wish' => $wish,
+            'budget_range' => $budget_range,
+            'content' => $content,
+            'contact_time' => $contact_time,
+            'website' => $website,
+            'prioritynum' => $prioritynum,
+            'updatetime' => $updatetime,
+        ]);
+        $Customer->update();
+               
+        //送出成功訊息
+        $this->load->model('Message');
+        $this->Message->show([
+            'message' => '設定成功',
+            'url' => 'admin/project/sales_target/sales_target/tablelist/'
+        ]);
+        return TRUE;
+    }
+
+    public function tablelist()
+    {
+        $data = $this->AdminModel->get_data(__FUNCTION__);
+
+        $data['search_customerid'] = $this->input->get('customerid');
+        $data['search_company'] = $this->input->get('company');
+        $data['search_customer_name'] = $this->input->get('customer_name');
+        $data['search_wish'] = $this->input->get('wish');
+        $data['search_contact_time'] = $this->input->get('contact_time');
+        $data['search_class_slug'] = $this->input->get('class_slug');
+        $data['search_budget_range'] = $this->input->get('budget_range');
+        $limitstart = $this->input->get('limitstart');
+        $limitcount = $this->input->get('limitcount');
+        $limitcount = !empty($limitcount) ? $limitcount : 20;
+
+        $class_ClassMeta = new ClassMeta([
+            'db_where_arr' => [
+                'slug' => $data['search_class_slug']
+            ]
+        ]);
+
+        $data['CustomerList'] = new ObjList([
+            'db_where_arr' => [
+                'customerid' => $data['search_customerid'],
+                'company like' => $data['search_company'],
+                'customer_name like' => $data['search_customer_name'],
+                'wish like' => $data['search_wish'],
+                'budget_range like' => $data['search_budget_range']
+            ],
+            'db_orderby_arr' => [
+                'prioritynum' => 'DESC',
+                'updatetime' => 'DESC'
+            ],
+            'db_where_deletenull_bln' => TRUE,
+            'obj_class' => 'Customer',
+            'limitstart' => $limitstart,
+            'limitcount' => $limitcount
+        ]);
+
+        $data['page_link'] = $data['CustomerList']->create_links(['base_url' => 'admin/'.$data['child1_name'].'/'.$data['child2_name'].'/'.$data['child3_name'].'/'.$data['child4_name']]);
+        
+        
+        $data['ClassMetaList'] = new ObjList([
+            'db_where_arr' => [
+                'modelname' => 'customer'
+            ],
+            'obj_class' => 'ClassMeta',
+            'limitstart' => 0,
+            'limitcount' => 100
+        ]);
+
+        //輸出模板
+        $this->load->view('admin/'.$data['admin_child_url'], $data);
+
+    }
+
+    public function tablelist_post()
+    {
+        $data = $this->AdminModel->get_data(__FUNCTION__);
+        $search_customerid = $this->input->post('search_customerid', TRUE);
+        $search_company = $this->input->post('search_company', TRUE);           
+        $search_class_slug = $this->input->post('search_class_slug', TRUE);
+        $search_customer_name = $this->input->post('search_customer_name', TRUE);
+        $search_wish = $this->input->post('search_wish', TRUE);
+        $search_contact_time = $this->input->post('search_contact_time', TRUE);
+        $search_budget_range = $this->input->post('search_budget_range', TRUE);
+        $search_updatetime = $this->input->post('search_updatetime', TRUE);
+        
+
+
+        $url = 'admin/project/sales_target/sales_target/tablelist/?';
+
+
+        if(!empty($search_customerid))
+        {
+            $url = $url.'&customerid='.$search_customerid;
+        }
+        
+        if(!empty($search_company))
+        {
+        	$url = $url.'&company='.$search_company;
+        }
+
+        if(!empty($search_customer_name))
+        {
+            $url = $url.'&customer_name='.$search_customer_name;
+        }
+        
+        if(!empty($search_wish))
+        {
+        	$url = $url.'&wish='.$search_wish;
+        }
+
+        if(!empty($search_contact_time))
+        {
+            $url = $url.'&contact_time='.$search_contact_time;
+        }
+        if(!empty($search_budget_range))
+        {
+            $url = $url.'&budget_range='.$search_budget_range;
+        }
+        if(!empty($search_updatetime))
+        {
+            $url = $url.'&updatetime='.$search_updatetime;
+        }
+        
+        //送出成功訊息
+        $this->load->model('Message');
+        $this->Message->show([
+            'message' => '資料存取中...',
+            'url' => $url
+        ]);
+    }
+
+    public function calendar()
+    {
+        $data = $this->AdminModel->get_data(__FUNCTION__);
+
+        $data['CustomerClassMetaList'] = new ObjList([
+            'db_where_arr' => [
+                'modelname' => 'customer'
+            ],
+            'obj_class' => 'ClassMeta',
+            'limitstart' => 0,
+            'limitcount' => 100
+        ]);
+              
+        //global
+        $data['global']['js'][] = 'tool/ckeditor/ckeditor.js';
+        $data['global']['js'][] = 'tool/jquery-ui-timepicker-addon/script.js';
+        $data['global']['js'][] = 'tool/jquery-ui-timepicker-addon/zh-tw.js';
+        $data['global']['js'][] = 'tool/jquery-ui-timepicker-addon/style.css';
+        $data['global']['js'][] = 'tool/fullcalendar/moment.min.js';
+        $data['global']['js'][] = 'tool/fullcalendar/fullcalendar.min.js';
+        $data['global']['js'][] = 'tool/fullcalendar/gcal.js';
+        $data['global']['js'][] = 'admin/project/customer/customer/calendar.js';
+
+        //輸出模板
+        $this->load->view('admin/'.$data['admin_child_url'], $data);
+
+    }
+
+    public function delete()
+    {
+        $data = $this->AdminModel->get_data(__FUNCTION__);
+
+        $url = FALSE;
+        $dont_change_page = $this->input->get('dont_change_page');
+        if( empty( $dont_change_page ) )
+        {
+            $url = 'admin/project/sales_target/sales_target/tablelist';
+        }
+
+        //CSRF過濾
+        if( $this->input->get('hash') == $this->security->get_csrf_hash() )
+        {
+            $customer = new customer([
+                'customerid' => $this->input->get('customerid')
+            ]);
+            $customer->delete();
+
+            $this->load->model('Message');
+            $this->Message->show([
+                'message' => '刪除成功',
+                'url' => $url
+            ]);
+        }
+        else
+        {
+            $this->load->model('Message');
+            $this->Message->show([
+                'message' => 'hash驗證失敗，請使用標準瀏覽器進行刪除動作',
+                'url' => $url
+            ]);
+        }
+    }
+}
+
+?>
---
+++ views/admin/project/sales_target/tablelist.php
@@ -0,0 +1,152 @@
+<?=$temp['header_up']?>
+<script>
+Temp.ready(function(){
+
+    $('#start_time').datetimepicker({
+        dateFormat: 'yy-mm-dd',
+        timeFormat: 'HH:mm:ss'
+    });
+
+    $('#end_time').datetimepicker({
+        dateFormat: 'yy-mm-dd',
+        timeFormat: 'HH:mm:ss'
+    });
+});
+</script>
+<?=$temp['header_down']?>
+<?=$temp['admin_header_bar']?>
+<h2><?=$child2_title?> - <?=$child3_title?></h2>
+<div class="contentBox contentTablelist allWidth">
+	<h3><?=$child3_title?> > <?=$child4_title?></h3>
+	<h4>請選擇欲修改之<?=$child3_title?></h4>
+	<div class="spanLine noneBg">
+        <div class="spanLineLeft">
+			<a href="admin/<?=$child1_name?>/<?=$child2_name?>/<?=$child3_name?>/edit" class="button">新增<?=$child3_title?></a>
+        </div>
+	</div>
+	<div class="spanLineTable">
+        <div class="spanLineTableContent">
+            <div class="spanLine tablelist tableTitle">
+                <div class="spanLineLeft text width100">
+        			專案ID
+                </div>
+                <div class="spanLineLeft text width300">
+        			專案名稱
+                </div>
+                <div class="spanLineLeft text width100">
+                    專案總金額
+                </div>
+                <div class="spanLineLeft text width100">
+                    已收款項
+                </div>
+                <div class="spanLineLeft text width100">
+                    付款進度 (%)
+                </div>
+                <div class="spanLineLeft text width100">
+                    開始日期
+                </div>
+                <div class="spanLineLeft text width100">
+                    結束日期
+                </div>
+                <div class="spanLineLeft text width150">
+                    專案分類標籤
+                </div>
+                <div class="spanLineLeft text width150">
+                    編輯操作
+                </div>
+        	</div>
+            <div class="spanLine tablelist">
+                <?php echo form_open("admin/$child1_name/$child2_name/$child3_name/{$child4_name}_post/") ?>
+                    <div class="spanLineLeft text width100">
+                        <input type="number" class="text" style="margin-left:-6px;" value="<?=!empty($search_projectid)?$search_projectid:''?>" name="search_projectid" placeholder="請填寫ID">
+                    </div>
+                    <div class="spanLineLeft text width300">
+                        <input type="text" class="text" style="margin-left:-6px;" value="<?=!empty($search_name)?$search_name:''?>" name="search_name" placeholder="請填寫專案名稱">
+                    </div>
+                    <div class="spanLineLeft text width100">
+                        <input type="number" class="text" style="margin-left:-6px;" value="<?=!empty($search_pay_price_total)?$search_pay_price_total:''?>" name="search_pay_price_total" placeholder="專案總金額">
+                    </div>
+                    <div class="spanLineLeft text width100">
+                        <input type="number" class="text" style="margin-left:-6px;" value="<?=!empty($search_pay_price_receive)?$search_pay_price_receive:''?>" name="search_pay_price_receive" placeholder="已收款項">
+                    </div>
+                    <div class="spanLineLeft text width100">
+                        <input type="number" class="text" style="margin-left:-6px;" value="<?=!empty($search_pay_price_schedule)?$search_pay_price_schedule:''?>" name="search_pay_price_schedule" placeholder="付款進度">
+                    </div>
+                    <div class="spanLineLeft text width100">
+                        <script src="js/fanswoo-framework/tool/jquery-ui-timepicker-addon/script.js"></script>
+                        <link rel="stylesheet" type="text/css" href="js/fanswoo-framework/tool/jquery-ui-timepicker-addon/style.css"></link>
+                        <input type="text" id="start_time" class="text" style="margin-left:-6px;" value="<?=!empty($search_setuptime)?$search_setuptime:''?>" name="search_start_time" placeholder="開始日期">
+                    </div>
+                    <div class="spanLineLeft text width100">
+                        <script src="js/fanswoo-framework/tool/jquery-ui-timepicker-addon/script.js"></script>
+                        <link rel="stylesheet" type="text/css" href="js/fanswoo-framework/tool/jquery-ui-timepicker-addon/style.css"></link>
+                        <input type="text" id="end_time" class="text" style="margin-left:-6px;" value="<?=!empty($search_endtime)?$search_endtime:''?>" name="search_end_time" placeholder="結束日期">
+                    </div>
+                    <div class="spanLineLeft text width150">
+                        <select name="search_class_slug" style="margin-left:-6px;">
+                            <option value="">不透過分類標籤篩選</option>
+                            <?foreach($ProjectClassMetaList->obj_arr as $key => $value_ClassMeta):?>
+                            <option value="<?=$value_ClassMeta->slug?>"<?if(!empty($search_class_slug) && $search_class_slug == $value_ClassMeta->slug) echo ' selected'?>><?=$value_ClassMeta->classname?></option>
+                            <?endforeach?>
+                        </select>
+                    </div>
+                    <div class="spanLineLeft text width150">
+                        <input type="submit" class="button" style="height: 30px; margin-left:-6px;" value="篩選">
+                    </div>
+                </form>
+            </div>
+            <?if(!empty($ProjectList->obj_arr)):?>
+            <?foreach($ProjectList->obj_arr as $key => $value_Project):?>
+            <div class="spanLine tablelist">
+                <div class="spanLineLeft text width100">
+                    <?=$value_Project->projectid?>
+                </div>
+                <div class="spanLineLeft text width300">
+                    <a href="admin/<?=$child1_name?>/<?=$child2_name?>/<?=$child3_name?>/edit/?projectid=<?=$value_Project->projectid?>">
+                        <?=$value_Project->name?>
+                    </a>
+                </div>
+                <div class="spanLineLeft text width100">
+                    NT $<span class="project_price"><?=$value_Project->pay_price_total?></span>
+                </div>
+                <div class="spanLineLeft text width100">
+                    NT $<?=$value_Project->pay_price_receive?>
+                </div>
+                <div class="spanLineLeft text width100">
+                    <?=$value_Project->pay_price_schedule?> %
+                </div>
+                <div class="spanLineLeft text width100">
+                    <?=$value_Project->setuptime_DateTimeObj->inputtime_date?>
+                </div>
+                <div class="spanLineLeft text width100">
+                   <?=$value_Project->endtime_DateTimeObj->inputtime_date?>
+                </div>
+                <div class="spanLineLeft text width150">
+                    <?if(!empty($value_Project->class_ClassMetaList->obj_arr)):?>
+                    <?foreach($value_Project->class_ClassMetaList->obj_arr as $key => $value_ClassMeta):?>
+                        <?if($key !== 0):?>,<?endif?><?=$value_ClassMeta->classname?>
+                    <?endforeach?>
+                    <?else:?>
+                    <span class="gray">沒有分類標籤</span>
+                    <?endif?>
+                </div>
+                <div class="spanLineLeft width150 tablelistMenu">
+                    <a href="admin/<?=$child1_name?>/<?=$child2_name?>/<?=$child3_name?>/edit/?projectid=<?=$value_Project->projectid?>">編輯</a>
+                    <a href="admin/<?=$child1_name?>/<?=$child2_name?>/<?=$child3_name?>/copy/?projectid=<?=$value_Project->projectid?>">複製</a>
+                    <span class="ahref" onClick="fanswoo.check_href_action('確定要刪除嗎？', 'admin/<?=$child1_name?>/<?=$child2_name?>/<?=$child3_name?>/delete/?projectid=<?=$value_Project->projectid?>&hash=<?=$this->security->get_csrf_hash()?>');">刪除</span>
+                </div>
+        	</div>
+            <?endforeach?>
+            <?else:?>
+            <div class="spanLine">
+                <div class="spanLineLeft text width500">
+                    這個篩選條件沒有搜尋到結果，請選擇其它篩選條件
+                </div>
+            </div>
+            <?endif?>
+        </div>
+    </div>
+    <div class="pageLink"><?=$page_link?></div>
+</div>
+<?=$temp['admin_footer_bar']?>
+<?=$temp['body_end']?>
