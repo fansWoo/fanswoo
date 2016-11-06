966448e182c665b2a14d371eb04433d706c4f73e
Sacriley <yi@fanswoo.com>
2016-11-06 19:58:35 +0800

    去除 FB 像素

--- views/temp/header_bar.php
+++ views/temp/header_bar.php
@@ -1,22 +1,3 @@
-<script>
-(function() {
-var _fbq = window._fbq || (window._fbq = []);
-if (!_fbq.loaded) {
-var fbds = document.createElement('script');
-fbds.async = true;
-fbds.src = '//connect.facebook.net/en_US/fbds.js';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(fbds, s);
-_fbq.loaded = true;
-}
-_fbq.push(['addPixelId', '694153537333853']);
-})();
-window._fbq = window._fbq || [];
-window._fbq.push(['track', 'PixelInitialized', {}]);
-</script>
-<noscript>
-	<img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=694153537333853&amp;ev=PixelInitialized" />
-</noscript>
 <script type="text/javascript">
 $(document).ready(function() {
 	$(window).scroll(function(){
